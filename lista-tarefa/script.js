44a1e90013cae750bc9e8eae52e5327a15a3c853
César Benfica <cesar.benfica.450@hotmail.com>
2022-08-28 21:10:07 -0300

    praticando CSS

--- lista-tarefa/script.js
+++ lista-tarefa/script.js
@@ -1,8 +1,11 @@
+let idTarefa = 0
+
 const inputTarefa = document.getElementById("tarefa-nova")
 
 function adicionarTarefa() {
     const item = document.createElement("li")
-
+    idTarefa++;
+    item.id = idTarefa;
     item.innerHTML = `<li>
             <span>${inputTarefa.value}</span>
             <button class="excluir">x</button>
@@ -10,7 +13,7 @@ function adicionarTarefa() {
 
     const lista = document.querySelector("#lista");
     lista.appendChild(item);
-    inputTarefa.value = ""
+    inputTarefa.value = "";
 }
 
 const botaoAdiconar = document.querySelector("#btn-adicionar")
@@ -18,8 +21,15 @@ botaoAdiconar.addEventListener("click", adicionarTarefa)
 
 function limparLista() {
     const lista = document.querySelector("#lista");
-    lista.innerHTML = ""
+    lista.innerHTML = "";
 }
 const botaoLimpar = document.querySelector(".bt-azul")
 botaoLimpar.addEventListener("click", limparLista)
 
+// function concluir(itemId) {
+//     const tarefaSelecionada = document.getElementById(itemId);
+//     tarefaSelecionada.className = "concluida";
+// }
+
+// item.innerHTML = `<span onclick="concluir(${inputTarefa.value}</span>)
+// <button class="feito" onclick ="concluido(event)">x</button>`;
